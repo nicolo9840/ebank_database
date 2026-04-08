a8bdffc7827ec724201cf05a5b465f642c824d62
Nicolò Rotella <nicolorotella@MacBookAir.fritz.box>
2026-04-08 17:06:47 +0200

    Add triggers

---
+++ sql/triggers.sql
@@ -0,0 +1,40 @@
+DELIMITER //
+CREATE TRIGGER check_balance_before_insert
+BEFORE INSERT ON Transactions
+FOR EACH ROW
+BEGIN
+   DECLARE current_balance DECIMAL(15,2);
+   SELECT balance INTO current_balance 
+   FROM Accounts 
+   WHERE account_id = NEW.from_account_id;
+   IF NEW.transaction_type IN ('Withdrawal', 'Transfer') THEN
+      IF current_balance < NEW.amount THEN
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Insufficient balance';
+      END IF;
+   END IF;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER update_balance
+AFTER INSERT ON Transactions
+FOR EACH ROW
+BEGIN
+    IF NEW.transaction_type = 'Deposit' THEN
+    UPDATE Accounts 
+    SET balance = balance + NEW.amount 
+    WHERE account_id = NEW.to_account_id;
+    END IF;
+    IF NEW.transaction_type = 'Withdrawal' THEN
+    UPDATE Accounts 
+    SET balance = balance - NEW.amount 
+    WHERE account_id = NEW.from_account_id;
+    END IF;
+    IF NEW.transaction_type = 'Transfer' THEN
+    UPDATE Accounts SET balance = balance - NEW.amount 
+    WHERE account_id = NEW.from_account_id;
+    UPDATE Accounts SET balance = balance + NEW.amount 
+    WHERE account_id = NEW.to_account_id;     
+    END IF;
+END //
+DELIMITER ;
