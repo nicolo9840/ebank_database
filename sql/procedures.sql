b3479cc66435b61b928690cb415ba8ed95743bf9
Nicolò Rotella <nicolorotella@MacBookAir.fritz.box>
2026-04-08 17:39:23 +0200

    Add procedures

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -2,7 +2,8 @@ DELIMITER //
 CREATE PROCEDURE TransferFunds( IN from_id INT, IN to_id INT, IN amount DECIMAL(15,2) ) 
 BEGIN 
    START TRANSACTION;   
-   IF (SELECT balance FROM Accounts WHERE account_id = from_id) < amount THEN                                         	ROLLBACK; 
+   IF (SELECT balance FROM Accounts WHERE account_id = from_id) < amount THEN
+        ROLLBACK; 
    ELSE 
    	INSERT INTO Transactions (transaction_type, amount, description, status, transaction_date, from_account_id, to_account_id) 
 	VALUES ('Transfer', amount, 'Transfer', 'Completed', NOW(), from_id, to_id); 
@@ -23,12 +24,12 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE FreezeAccount(IN acc_id INT)
 BEGIN
-DECLARE current_status VARCHAR(20);
-SELECT status INTO current_status FROM Accounts WHERE account_id = acc_id;
-IF current_status = 'Active' THEN
-UPDATE Accounts SET status = 'Frozen' WHERE account_id = acc_id;
-ELSE
-SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Account is not active';
-END IF;
+   DECLARE current_status VARCHAR(20);
+   SELECT status INTO current_status FROM Accounts WHERE account_id = acc_id;
+   IF current_status = 'Active' THEN
+      UPDATE Accounts SET status = 'Frozen' WHERE account_id = acc_id;
+   ELSE
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Account is not active';
+   END IF;
 END //
 DELIMITER ;
